99ffbf5e13211b9325c03c621ae1d803d1e74119
Malind Kowalski <malind.nikulski@gmail.com>
2019-03-26 17:14:29 -0600

    basic index file setup

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //2. component App code
+const App = () => {
+  return (
+    <div>
+      Welcome to the Timezone App
+    </div>
+  )
+}
 
 //3. display content on browser
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
