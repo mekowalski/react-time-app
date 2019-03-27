258f44e3f11521fecadb2d8aa42a5f27c769997d
Malind Kowalski <malind.nikulski@gmail.com>
2019-03-26 18:03:11 -0600

    returned timestamp via milli-second

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
   window.navigator.geolocation.getCurrentPosition(
     position => console.log(position.timestamp)
+    // => 1553644528444
 
   )
   return (
