522ebfb8bce7f7dbae3351741adde208c94093a7
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-02 14:15:51 -0600

    minor notes on App component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //2. component App code
+//change to class-based comp and utilize either constructor for setting date
+//or component life cycle (componentDidMount()) for `getDate()`
+//then return in render() call
 const App = () => {
   window.navigator.geolocation.getCurrentPosition(
     position => console.log(position.timestamp),
