f5bd8bab233cea1b90e04de9cb93fb84af62aaf1
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-03 15:32:01 -0600

    add necessary tasks for TimeContent

--- src/TimeContent.js
+++ src/TimeContent.js
@@ -9,3 +9,7 @@ const TimeContent = () => {
 }
 
 export default TimeContent
+
+//1. configure the time of day here
+//2. get the time of day via function
+//3. display the content of the time of day text and icons
