722c0a441b438e70c92d8df1d71e57c8f1fd169c
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-04 11:28:39 -0600

    comment constructor() and only use default state, works fine

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   //default state set up
-  //state = { date: '' }
+  //this also works without constructor()!
+  state = { date: new Date().toLocaleTimeString() }
 
   //React does not require constuctor(), more of a function belonging to JS language
   //Anytime a new instance of App Component is created, constructor() will be called before anything else
   //perfect location to initialize state
-  constructor() {
-    super() //App borrows from React.Component based class
-    this.state = {
-      date: new Date().toLocaleTimeString()
-    }
-  }
+  // constructor() {
+  //   super() //App borrows from React.Component based class
+  //   this.state = {
+  //     date: new Date().toLocaleTimeString()
+  //   }
+  // }
 
   //React requires a render() method to return JSX
   render() {
