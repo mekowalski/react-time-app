c326953ded81e391e8a3fb6e715a773e05e7efe8
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-04 14:52:07 -0600

    remove comments on life cycle

--- src/index.js
+++ src/index.js
@@ -4,16 +4,6 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   state = { date: new Date().toLocaleTimeString() }
 
-  //deciding if life cycle is necessary
-  //initial data-loading
-  //best practice, do not data-load in constructor(), you can BUT it's recommended in componentDidMount()
-  //state techinically only changes when new time is retrieved
-  //really don't know if i'll use this life cycle at all
-  //would i use this to 'setState' initally?  gah never know about life cycles!!!
-  componentDidMount() {
-    console.log('component rendered to the screen')
-  }
-
   render() {
     return (
       <div>
