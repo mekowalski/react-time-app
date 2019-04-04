361ad45c3cec43dd201967a8f8add833ed8871bc
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-04 11:55:35 -0600

    thinking of changing app name

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  //default state set up
-  //this also works without constructor()!
   state = { date: new Date().toLocaleTimeString() }
 
-  //React requires a render() method to return JSX
   render() {
     return (
       <div>
-        Welcome to the Timezone App
+        Welcome to the Time App
         <p>Timestamp: {this.state.date}</p>
       </div>
     )
